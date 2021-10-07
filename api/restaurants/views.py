fa8f5ad129ff4648d97c27f679a8420a90b92d1d
donghyun0218 <dhkim20157@8percent.kr>
2021-10-08 03:29:57 +0900

    ADD: 검색 기능 추가

--- api/restaurants/views.py
+++ api/restaurants/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from api.restaurants.serializers import RestaurantSerializer
 from apps.restaurants.models import Restaurant
@@ -7,3 +7,5 @@ from apps.restaurants.models import Restaurant
 class RestaurantViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'address']
