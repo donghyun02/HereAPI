b2bf1af02f15cd07edb738ba251ad42a7a017a65
donghyun0218 <dhkim20157@8percent.kr>
2021-10-08 02:48:05 +0900

    CHG: id 값도 직렬화 하도록 수정

--- api/restaurants/serializers.py
+++ api/restaurants/serializers.py
@@ -14,6 +14,7 @@ class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = (
+            'id',
             'name',
             'image',
             'address',
