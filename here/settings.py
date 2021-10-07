3d985d23e0ac33e94cab45fc9395f2b6949c0c68
donghyun0218 <dhkim20157@8percent.kr>
2021-10-07 23:06:34 +0900

    ADD: 식당 앱 추가

--- here/settings.py
+++ here/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'apps.users.apps.UsersConfig'
+    'apps.users.apps.UsersConfig',
+    'apps.restaurants.apps.RestaurantsConfig',
 ]
 
 MIDDLEWARE = [
